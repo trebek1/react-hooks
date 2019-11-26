fd882a2c33ba542b50c9a0b6bbc104ceba586abd
Alex Trzeciak <trebek08@gmail.com>
2019-11-25 21:59:49 -0800

    useState

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
 const IngredientForm = React.memo(props => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
   };
 
   return (
@@ -15,11 +17,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={({ target: { value } }) => setTitle(value)}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              onChange={({ target: { value } }) => setAmount(value)}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
